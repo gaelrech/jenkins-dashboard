5aeef76b2c81175ff043ec66a806995aa1f6e16a
Gabriel Rech <gaelrech@gmail.com>
2020-10-10 16:37:16 -0300

    Updates

---
+++ precache-manifest.5a4df23164ef477376ba8bb94f26e52c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d830b63670b64c4ec419a4afe75230b6",
+    "url": "/jenkins-dashboard/index.html"
+  },
+  {
+    "revision": "821234d076426d6e3e26",
+    "url": "/jenkins-dashboard/static/js/2.69cf6b7f.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/jenkins-dashboard/static/js/2.69cf6b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd9f1e0c043d8e10deb",
+    "url": "/jenkins-dashboard/static/js/main.3745b87e.chunk.js"
+  },
+  {
+    "revision": "a9a2b71f4d590025c8e8",
+    "url": "/jenkins-dashboard/static/js/runtime-main.28f695d0.js"
+  }
+]);
